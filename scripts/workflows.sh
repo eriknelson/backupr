80d2fec4db661d6249098008b314741e5242afa6
Erik Nelson <erik@nsk.io>
2023-04-09 00:17:03 -0400

    Add act workflow script

---
+++ scripts/workflows.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+act -v -s B2_KEY_NAME="$B2_KEY_NAME" \
+  -s B2_BUCKET_NAME="$B2_BUCKET_NAME" \
+  -s B2_APPLICATION_KEY_ID="$B2_APPLICATION_KEY_ID" \
+  -s B2_APPLICATION_KEY="$B2_APPLICATION_KEY"
