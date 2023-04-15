ae7e5d62533ac4c6a9a7366873781e6b960c882e
Erik Nelson <erik@nsk.io>
2023-04-15 13:41:11 -0400

    Add deep integration test (#4)
    * Make a note about deep integration tests
    
    * Rename Dockerfile and update pipeline
    
    * Experiment with act run python script in workflow
    
    * Checkpoint file
    
    * Remove unused shell script
    
    * Update to lint the deep-int-test
    
    * Add docker sdk to dev deps
    
    * Update debug to support deep_int_test
    
    * Update docker to actually run backupr
    
    * Also fix a bug that installs gpg deps for the image
    
    * Fix workflow name
    
    * Use correct name for int test
    
    * Pass the linter
    
    * Add a todo for integrating the deep-int-test
    
    * Should integrate the deep int test with the pipeline
    
    * Include deep int test in tests
    
    * Change name of deep integration test file comply
    
    * Update deep int so runs with workflow
    
    * Add ci script for deep-int-test
    
    * Ignore covereage file from commits
    
    * Update dep files
    
    * Add the pipeline

--- debug.py
+++ debug.py
@@ -1,8 +1,15 @@
 import os
+from loguru import logger
 
 import pytest
 test_spec = os.getenv('TEST_SPEC')
-if test_spec:
+deep_integration_test = os.getenv('DEEP_INTEGRATION_TEST')
+if deep_integration_test:
+    logger.info('debug.deep_integration_test')
+    retcode = pytest.main(["-s", 'scripts/ci/deep-int-test.py'])
+elif test_spec:
+    logger.info(f'debug.test_spec -> {test_spec}')
     retcode = pytest.main(["-s", f'{test_spec}'])
 else:
+    logger.info('debug.default tests')
     retcode = pytest.main(["-s"])
